dae872bb97e3188aa4750efb4ed5fc697897290b
Stevan Goode <steve@madhouseminers.com>
2020-08-22 17:12:20 +0100

    Deleted session data from redis on logout

--- src/server/web/handlers/logout.ts
+++ src/server/web/handlers/logout.ts
@@ -1,6 +1,19 @@
 import express from "express";
 
-export default (req: express.Request, res: express.Response) => {
+import redis from "redis";
+const client = redis.createClient();
+
+import { promisify } from "util";
+const delAsync = promisify(client.del).bind(client);
+
+export default async (req: express.Request, res: express.Response) => {
+  const matches = req.headers.cookie?.match("LOGIN_TOKEN=(.*);");
+  if (matches) {
+    const session_token = matches[1];
+    // @ts-ignore
+    await delAsync(session_token);
+  }
+
   res.clearCookie("LOGIN_TOKEN");
   res.clearCookie("LOGGED_IN");
   res.redirect("/");
