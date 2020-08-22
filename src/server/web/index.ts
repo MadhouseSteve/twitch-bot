7f71c8cdb7956fde4c6393e6fdfda8ca786cd8f4
Stevan Goode <steve@madhouseminers.com>
2020-08-22 11:41:40 +0100

    Moved index to a static directory

--- src/server/web/index.ts
+++ src/server/web/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 
 app.use(express.static("output")); // TODO: Add some tests for this stuff
+app.use(express.static("static")); // TODO: Add some tests for this stuff
 app.use((req, res, next) => res.status(404).send("Not Found"));
 
 export default app;
